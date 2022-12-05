ee304ac0bdc09dc1e2c1807c6cfb91ed13fe5dc6
Brian Crant <17074379+bcrant@users.noreply.github.com>
2022-12-04 18:07:34 -0700

    Day 1

---
+++ day1.py
@@ -0,0 +1,38 @@
+import pprint
+from collections import defaultdict
+
+
+def part1():
+    with open('./day1_input.txt', 'r') as f:
+        items = f.read().splitlines()
+
+    dd = defaultdict(list)
+    total_calories = {}
+    elf_num = 1
+    for item in items:
+        if item != '':
+            dd[elf_num].append(int(item))
+        else:
+            total_calories[elf_num] = sum(dd[elf_num])
+            elf_num += 1
+    print(dd)
+    pprint.pprint(total_calories)
+
+    max_cals = (0, 0)
+    for k, v in total_calories.items():
+        if v > max_cals[1]:
+            max_cals = (k, v)
+            print(f"elf {k} with {v} calories")
+    print(f"max_cals: {max_cals}")
+    return total_calories
+
+
+def part2(calories_by_elf: dict):
+    max_calories = list(reversed(sorted([i for i in calories_by_elf.values()])))
+    print(f"The three elves carrying the most calories have a total of {sum(max_calories[0:3])} calories")
+    return sum(max_calories[0:3])
+
+
+if __name__ == '__main__':
+    calories = part1()
+    part2(calories)
