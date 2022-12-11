42158a595a11d79662c10b96b3762217607add51
Brian Crant <17074379+bcrant@users.noreply.github.com>
2022-12-10 17:23:12 -0700

    Checkpoint.

--- day8.py
+++ day8.py
@@ -6,8 +6,9 @@ from collections import defaultdict
 
 def part1():
     with open("./data/day8_example.txt", "r") as f:
-        items = [list(map(int, x)) for x in f.read().splitlines()]
-    pprint.pprint(items)
+        items = [x for x in f.read().splitlines()]
+    for item in items:
+        print(item)
 
     rows = len(items)
     cols = len(items[0])
@@ -16,15 +17,15 @@ def part1():
 
     # ROWS
     rows_lr = items
-    rows_rl = [[*reversed(row)] for row in items]
+    rows_rl = ["".join([*reversed(row)]) for row in items]
 
     # COLS
     cols_tb = []
     cols_bt = []
     for col_idx in range(len(items[0])):
         col = [row[col_idx] for row_idx, row in enumerate(items)]
-        cols_tb.append(col)
-        cols_bt.append([*reversed(col)])
+        cols_bt.append("".join([*reversed(col)]))
+        cols_tb.append("".join([*col]))
 
     views = {
         "COLS_FROM_BOTTOM": cols_bt,
@@ -34,15 +35,71 @@ def part1():
     }
     pprint.pprint(views)
 
-    visible = defaultdict(int)
-    for view, height_records in views.items():
-        print(view)
-        for heights in height_records:
-            for height in heights:
-                if height > heights[0]:
-                    visible[view] += 1
-    pprint.pprint(visible)
+    visible = defaultdict(tuple)
+    row_pos = 0
+    col_pos = 0
+    for row_idx, row in enumerate(items):
+        for col_idx, col in enumerate(row):
+            print(f"row{row_idx+1} col{col_idx+1}: {row[col_idx]}")
+            curr = row[col_idx]
+            for view, items in views.items():
+                if "ROWS" in view:
+                    is_visible = curr < items[row_idx][col_idx - 1]
+                    print(f"curr {curr} < item {items[row_idx][col_idx - 1]}: ", is_visible)
+                    print(f"Visible from {view.rsplit('_')[-1]}: {is_visible}")
+                    # if curr > items[row_idx][col_idx + 1]:
+                    # print(items[row_idx][col_idx])
+                    # print(f"Visible from {view.rsplit('_')[-1]}: ")
+                    # for n in items[row_idx]:
+                    #     # if n >
+                    #     # items[row_idx] > items
+                    #     print(idx, height)
+                    #     # This excludes the perimeter values from being counted
+                    #     if height > heights[0]:
+                    #         visible[view].append((idx, height))
+
+
+    # visible = defaultdict(list)
+    # for view, height_records in views.items():
+    #     for heights in height_records:
+    #         print(view, heights)
+    #         for idx, height in enumerate(heights):
+    #             print(idx, height)
+    #             # This excludes the perimeter values from being counted
+    #             if height > heights[0]:
+    #                 visible[view].append((idx, height))
+    # pprint.pprint(visible)
 
 
 if __name__ == "__main__":
     print(f"part1 answer: {part1()}")
+
+
+    # # ROWS
+    # rows_lr = items
+    # rows_rl = [[*reversed(row)] for row in items]
+    #
+    # # COLS
+    # cols_tb = []
+    # cols_bt = []
+    # for col_idx in range(len(items[0])):
+    #     col = [row[col_idx] for row_idx, row in enumerate(items)]
+    #     cols_tb.append(col)
+    #     cols_bt.append([*reversed(col)])
+    #
+    # views = {
+    #     "COLS_FROM_BOTTOM": cols_bt,
+    #     "COLS_FROM_TOP": cols_tb,
+    #     "ROWS_FROM_LEFT": rows_lr,
+    #     "ROWS_FROM_RIGHT": rows_rl,
+    # }
+    # pprint.pprint(views)
+    #
+    # visible = defaultdict(int)
+    # for view, height_records in views.items():
+    #     print(view)
+    #     for heights in height_records:
+    #         for height in heights:
+    #             if height > heights[0]:
+    #                 visible[view] += 1
+    # pprint.pprint(visible)
