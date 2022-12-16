c6879c803467ecc679d2eaeb4f62947be263c2e8
Brian Crant <17074379+bcrant@users.noreply.github.com>
2022-12-15 18:14:17 -0700

    Checkpoint.

--- day8.py
+++ day8.py
@@ -28,8 +28,8 @@ def part1():
         cols_tb.append("".join([*col]))
 
     views = {
-        "COLS_FROM_BOTTOM": cols_bt,
-        "COLS_FROM_TOP": cols_tb,
+        # "COLS_FROM_BOTTOM": cols_bt,
+        # "COLS_FROM_TOP": cols_tb,
         "ROWS_FROM_LEFT": rows_lr,
         "ROWS_FROM_RIGHT": rows_rl,
     }
@@ -37,53 +37,49 @@ def part1():
 
     # Count the  number of trees that are visible from outside the grid
     visible = defaultdict(int)
-    for row_idx, row in enumerate(items[1:-1]):
+    for row_idx, row in enumerate(items):
         print()
         print(f"ROW     {row_idx}  | {row}")
         for col_idx, curr in enumerate(row):
             pos = f"x{row_idx}y{col_idx}"
-            print(f"CURR {pos}  | {' '.join(seq)} < {curr}")
-            # LEFT TO RIGHT
-            seq = row[0:col_idx] or row[0]
-            is_visible = all(n < curr for n in seq)
-            if not visible[pos]:
-                visible[pos] = 1 if is_visible else 0
+            for view, trees in views.items():
+                print(f"VIEW {view} | {trees}")
+                left_seq = trees[row_idx][0:col_idx] or trees[row_idx][col_idx]
+                right_seq = trees[row_idx][col_idx:] or trees[row_idx][col_idx]
+
+                left_visible = all(n < curr for n in left_seq)
+                print(f"CURR {pos}  | {' '.join(left_seq)} < {curr}")
+                print(f"left visible: {left_visible}")
+                right_visible = all(n > curr for n in right_seq)
+                print(f"CURR {pos}  | {' '.join(right_seq)} > {curr}")
+                print(f"right visible: {right_visible}")
+                is_visible = 1 if left_visible or right_visible else 0
+                print(f"is visible: {bool(is_visible)}")
+
+                if not visible[pos]:
+                    visible[pos] = 1 if is_visible else 0
     pprint.pprint(visible)
-    inner_trees = sum(visible.values())
+
+    inner_visible = {}
+    for k, v in visible.items():
+        if not any(outer in k for outer in ("x0", "x4", "y0", "y4")):
+            inner_visible[k] = v
+    pprint.pprint(inner_visible)
+
+    inner_trees = sum(inner_visible.values())
     print(f"Inner trees: {inner_trees}")
     print(f"Outer trees: {outer_trees}")
     cnt_visible = inner_trees + outer_trees
     print(f"Visible: {cnt_visible}")
-            # curr_visible = False
-            # print(f"x{row_idx}y{col_idx} | {curr}")
-            # seq = row[row_idx][0:col_idx]
-            # print(f"curr {curr} seq {seq}")
-
-                    # is_visible = curr < items[row_idx][col_idx - 1]
-                    # print(f"curr {curr} < item {items[row_idx][col_idx - 1]}: ", is_visible)
-                    # print(f"Visible from {view.rsplit('_')[-1]}: {is_visible}")
-                    # if curr > items[row_idx][col_idx + 1]:
-                    # print(items[row_idx][col_idx])
-                    # print(f"Visible from {view.rsplit('_')[-1]}: ")
-                    # for n in items[row_idx]:
-                    #     # if n >
-                    #     # items[row_idx] > items
-                    #     print(idx, height)
-                    #     # This excludes the perimeter values from being counted
-                    #     if height > heights[0]:
-                    #         visible[view].append((idx, height))
 
 
-    # visible = defaultdict(list)
-    # for view, height_records in views.items():
-    #     for heights in height_records:
-    #         print(view, heights)
-    #         for idx, height in enumerate(heights):
-    #             print(idx, height)
-    #             # This excludes the perimeter values from being counted
-    #             if height > heights[0]:
-    #                 visible[view].append((idx, height))
-    # pprint.pprint(visible)
+# # LEFT TO RIGHT
+# seq = row[0:col_idx] or row[0]
+# print(f"seq: {seq}")
+# is_visible = all(n < curr for n in seq)
+# # print(f"CURR {pos}  | {' '.join(seq)} < {curr}")
+# if not visible[pos]:
+#     visible[pos] = 1 if is_visible else 0
 
 
 if __name__ == "__main__":
