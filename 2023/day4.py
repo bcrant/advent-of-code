29c1a64f0b5eb326e3e005152aba4d7609d0808b
Brian Crant <17074379+bcrant@users.noreply.github.com>
2023-12-13 08:28:02 -0700

    2023 Day 4.

---
+++ 2023/day4.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+from pprint import pprint
+
+
+year, day = 2023, 4
+
+
+def part1():
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = [
+            [i.strip().split() for i in item.split(":")[1].split("|")]
+            for item in f.read().splitlines()
+        ]
+    # pprint(items)
+
+    points = 0
+    cards = defaultdict(int)
+    for game, card in enumerate(items):
+        cards[game] += 1
+        winners = [int(n) for n in card[0]]
+        picks = [int(n) for n in card[1]]
+        cnt = len(set(winners) & set(picks))
+        # print(f'cnt {type(cnt)}: {cnt}')
+        if cnt > 0:
+            points += 2 ** (cnt - 1)
+        for i in range(cnt):
+            cards[game + 1 + i] += cards[game]
+    return points
+
+
+def part2():
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = [
+            [i.strip().split() for i in item.split(":")[1].split("|")]
+            for item in f.read().splitlines()
+        ]
+
+    points = 0
+    cards = defaultdict(int)
+    for game, card in enumerate(items):
+        cards[game] += 1
+        winners = [int(n) for n in card[0]]
+        picks = [int(n) for n in card[1]]
+        cnt = len(set(winners) & set(picks))
+        # print(f'cnt {type(cnt)}: {cnt}')
+        if cnt > 0:
+            points += 2 ** (cnt - 1)
+        for i in range(cnt):
+            cards[game + 1 + i] += cards[game]
+    return sum(cards.values())
+
+
+if __name__ == "__main__":
+    print(f"part1 answer: {part1()}")
+    print(f"part2 answer: {part2()}")
