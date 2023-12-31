b8ce273cfb51f9a1cdc7f85e4e3e8e43daac092b
Brian Crant <17074379+bcrant@users.noreply.github.com>
2023-12-30 22:46:31 -0700

    2023 Day 9.

--- 2023/day9.py
+++ 2023/day9.py
@@ -6,24 +6,21 @@ year, day = 2023, 9
 
 def part1():
     with open(f"{year}/data/day{day}_input.txt", "r") as f:
-        items = [
-            [int(i) for i in item.split()]
-            for item in f.read().splitlines()
-        ]
+        items = [[int(i) for i in item.split()] for item in f.read().splitlines()]
     # pprint(items)
     print()
 
     answer = 0
     for item in items:
-        # print()
-        # print(f'item {type(item)}: {item}')
+        print("\n\n")
+        print(item)
         diffs = [item]
         seq = item
         solved = False
         while not solved:
             diff = []
-            for i in range(0, len(seq)-1):
-                diff.append(seq[i+1] - seq[i])
+            for i in range(0, len(seq) - 1):
+                diff.append(seq[i + 1] - seq[i])
             diffs.append(diff)
             if sum(diff) == 0:
                 solved = True
@@ -32,7 +29,7 @@ def part1():
 
         # This is just for pretty printing
         for idx, d in enumerate(diffs):
-            print(idx*" ", *d)
+            print(idx * " ", *d)
         print()
 
         rev_diffs = list(reversed(diffs))
@@ -40,21 +37,35 @@ def part1():
         for idx in range(1, len(rev_diffs)):
             rev_diffs[idx] = [
                 *rev_diffs[idx],
-                +rev_diffs[idx-1][-1] + rev_diffs[idx][-1]
+                +rev_diffs[idx - 1][-1] + rev_diffs[idx][-1],
             ]
 
         # This is also just for pretty printing
         for idx, d in enumerate(rev_diffs):
-            print((len(rev_diffs)-1-idx)*" ", *d)
-        
+            print((len(rev_diffs) - 1 - idx) * " ", *d)
+
         answer += rev_diffs[-1][-1]
     return answer
 
 
-# def part2():
-#     with open(f"{year}/data/day{day}_input.txt", "r") as f:
-#         items = f.read().splitlines()
-#     return
+def part2():
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+
+    def extrapolate(array):
+        if all(x == 0 for x in array):
+            return 0
+
+        deltas = [y - x for x, y in zip(array, array[1:])]
+        diff = extrapolate(deltas)
+        return array[0] - diff
+
+    total = 0
+    for line in items:
+        nums = list(map(int, line.split()))
+        total += extrapolate(nums)
+
+    return total
 
 
 if __name__ == "__main__":
@@ -63,4 +74,4 @@ if __name__ == "__main__":
     # correct answer = 1992273652
     #                = -37
     # I am not 100% convinced that my answer is wrong.
-    # print(f"part2 answer: {part2()}")
+    print(f"part2 answer: {part2()}")
