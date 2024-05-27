359fbdceaf09c4d17ffa05ab9dabf5feea5c4f01
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-05-26 20:20:29 -0700

    Copy Jonathan Paulson's day10 solution so I can proceed to Day 11.

---
+++ 2023/day11.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+
+year, day = 2023, 0
+
+
+def part1():
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+    pprint(items)
+    return
+
+
+def part2():
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+    return
+
+
+if __name__ == "__main__":
+    print(f"part1 answer: {part1()}")
+    print(f"part2 answer: {part2()}")
