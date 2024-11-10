9b0e19cc4edbe1298bbecf31ea183aa775f1cf8f
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-11-10 16:54:48 -0700

    2019 day1

---
+++ 2019/day1.py
@@ -0,0 +1,34 @@
+from math import floor
+from pprint import pp
+
+
+year, day = 2019, 1
+
+
+def equation(num: str) -> int:
+    return floor(int(num) / 3) - 2
+
+def part1():
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+    acc = 0
+    for mass in items:
+        acc += equation(mass)
+    return acc
+
+
+def part2():
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+    acc = 0
+    for mass in items:
+        tmp = equation(mass)
+        while tmp > 0:
+            acc += tmp
+            tmp = equation(tmp)
+    return acc
+
+
+if __name__ == "__main__":
+    print(f"part1 answer: {part1()}")
+    print(f"part2 answer: {part2()}")
