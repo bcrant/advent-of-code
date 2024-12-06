7862e98a9be470980c2735feca0af345d6333b26
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-05 23:27:41 -0700

    Checkpoint.

--- 2024/day4.py
+++ 2024/day4.py
@@ -5,12 +5,28 @@ YEAR, DAY = 2024, 4
 
 
 def part1(items):
-    pp(items)
+    # 1. Initialize a counter
+    cnt = 0
+    # 2. Create vertical-down slice
+    v_downs = []
+    # v_ups = [v_down.reverse() for v_down in v_downs]
+    h_rights = []
+    for row_idx, row in enumerate(items):
+        h_rights.append([i for i in row])
+        for col_idx, col in enumerate(row):
+            v_downs.append(col)
+    print(f'v_downs {type(v_downs)}:')
+    pp(v_downs)
+    print(f'h_rights {type(h_rights)}:')
+    pp(h_rights)
+    # 3. Reverse vertical-down slices for vertical-up
+    # 4. Create horizontal-right, down, left, right, diagonal left, diagonal right slices
+    # 3. Count all occurrences of "xmas" in each slice
+    # 4. Return 
     return
 
 
 def part2(items):
-    pp(items)
     return
 
 
