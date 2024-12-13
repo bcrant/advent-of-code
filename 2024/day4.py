04afd6e81c3639a15010de9f99cffc445731a0e6
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-12 21:19:30 -0700

    Checkpoint. Day 4.

--- 2024/day4.py
+++ 2024/day4.py
@@ -24,9 +24,8 @@ MOVES = {
     "SW": MOVE_SW
 }
 
+
 def get_adjacent_values(grid: list[list[str]], curr_pos: Tuple[int, int]) -> dict[Tuple[int, int], Union[None, str]]:
-    n_cols, n_rows = len(items[0]), len(items)
-    min_size = min(n_rows, n_cols)
     adjacent = {}
     for direction, directive in MOVES.items():
         next_pos = move(curr_pos, directive)
@@ -81,97 +80,65 @@ def part1(items):
             curr_val = val
             if curr_val == "X":
                 adjacent = get_adjacent_values(items, curr_pos)
-                for _, values in adjacent.items():
+                for _, v in adjacent.items():
                     if (
-                        values["next_val"] == "M" 
-                        and values["next_next_val"] == "A"
-                        and values["next_next_next_val"] == "S"
+                        v["next_val"] == "M" 
+                        and v["next_next_val"] == "A"
+                        and v["next_next_next_val"] == "S"
                     ):
                         cnt += 1
     return cnt
 
-    # # 2. Create vertical-down and horiztonal-right slices
-    # v_downs = defaultdict(list)
-    # h_rights = defaultdict(list)
-    # for row_idx, row in enumerate(items):
-    #     h_rights[row_idx] = "".join(row)
-    #     for col_idx, col in enumerate(row):
-    #         v_downs[col_idx].append(col)
 
-    # # Flatten to list of strings
-    # v_downs = ["".join(v_down) for v_down in v_downs.values()]
-    # h_rights = ["".join(h_right) for h_right in h_rights.values()]
-
-    # # 3. Reverse vertical-down slices for vertical-up
-    # v_ups = [reverse_str(v_down) for v_down in v_downs]
-    # h_lefts = [reverse_str(h_right) for h_right in h_rights]
-
-    # # Print debugging steps 1-3
-    # print(f'v_downs {type(v_downs)}:')
-    # pp(v_downs)
-    # print(f'h_rights {type(h_rights)}:')
-    # pp(h_rights)
-    # print(f'v_ups {type(v_ups)}')
-    # pp(v_ups)
-    # print(f'h_lefts {type(h_lefts)}')
-    # pp(h_lefts)
-
-    # # # 4. Create horizontal-right, down, left, right, diagonal left, diagonal right slices
-    # n_cols, n_rows = len(items[0]), len(items)
-    # min_size = min(n_rows, n_cols)
-    # diagonal_rights = [(i, i) for i in range(min_size)]
-    # # diagonal_rights = [items[i][i] for i in range(size)]
-    # print(f'diagonal_rights {type(diagonal_rights)}')
-    # pp(diagonal_rights)
-
-    # diagonal_lefts = [(i, n_cols - 1 - i) for i in range(min_size)]
-    # # diagonal_lefts = [items[i][n_cols - 1 - i] for i in range(size)]
-    # print(f'diagonal_lefts {type(diagonal_lefts)}:')
-    # pp(diagonal_lefts)
-
-    # fc_drs = defaultdict(list)
-    # fr_drs = defaultdict(list)
-    # fc_dls = defaultdict(list)
-    # fr_dls = defaultdict(list)
-    # for idx in range(n_cols):
-    #     fc_point = (idx, 0)
-    #     fr_point = (0, idx)
-
-    #     # First column move diagonal down right
-    #     fc_dr = [move(fc_point, (i, i)) for i in range(min_size)]
-    #     fc_drs[fc_point].extend(fc_dr)
-
-    #     # First row move diagonal down right
-    #     fr_dr = [move(fr_point, (i, i)) for i in range(min_size)]
-    #     fr_drs[fr_point].extend(fr_dr)
-
-    #     # # First column move diagonal down left
-    #     fc_dl = [move(fc_point, (i, n_cols - 1 - i)) for i in range(min_size)]
-    #     fc_dls[fc_point].extend(fc_dl)
-
-    #     # First row move diagonal down left
-    #     fr_dl = [move(fr_point, (i, n_cols - 1 - i)) for i in range(min_size)]
-    #     fr_dls[fr_point].extend(fr_dl)
-
-    # print(f"fc_drs {type(fc_drs)}")
-    # pp(dict(fc_drs))
-
-    # print(f"fr_drs {type(fr_drs)}")
-    # pp(dict(fr_drs))
-
-    # print(f"fc_dls {type(fc_dls)}")
-    # pp(dict(fc_dls))
-
-    # print(f"fr_dls {type(fr_dls)}")
-    # pp(dict(fr_dls))
-
-    # # 3. Count all occurrences of "xmas" in each slice
-    # # 4. Return
-    return
+def get_immediate_adjacent_values(grid: list[list[str]], curr_pos: Tuple[int, int]) -> dict[Tuple[int, int], Union[None, str]]:
+    adjacent = {}
+    for direction, directive in MOVES.items():
+        next_pos = move(curr_pos, directive)
+        if is_negative(next_pos):
+            continue
+        next_val = get_value_if_exists(grid, next_pos)
+        adjacent[next_pos] = {
+            "direction": direction, 
+            "next_pos": next_pos, 
+            "next_val": next_val, 
+        }
+    return adjacent
 
 
 def part2(items):
-    return
+    items = [[i for i in it] for it in items]
+    pp(items)
+
+    # 1. Initialize a counter
+    cnt = 0
+
+    OPPOSITE_DIRECTIONS = {
+        "NE": "SW",
+        "NW": "SE",
+        "SW": "NE",
+        "SE": "NW",
+    }
+    OPPOSITE_CHARACTER = {
+        "M": "S",
+        "S": "M",
+    }
+
+    # 2.
+    for y, row in enumerate(items):
+        for x, val in enumerate(row):
+            curr_pos = (x, y)
+            curr_val = val
+            if curr_val == "A":
+                adjacent = get_immediate_adjacent_values(items, curr_pos)
+                pp(adjacent)
+                for _, v in adjacent.items():
+                    if (
+                        True 
+                        and ((v["direction"] == "NE" and v["next_val"] == "M") and (v["direction"] == "SW" and v["next_val"] == "S"))
+                        and ((v["direction"] == "NW" and v["next_val"] == "M") and (v["direction"] == "SE" and v["next_val"] == "S"))
+                    ):
+                        cnt += 1
+    return cnt
 
 
 def move(point: Tuple[int, int], direction: Tuple[int, int]) -> Tuple[int, int]:
@@ -192,5 +159,5 @@ def read_input(year: int, day: int) -> list:
 
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
-    print(f"part1 answer: {part1(items)}")
-    # print(f"part2 answer: {part2(items)}")
+    # print(f"part1 answer: {part1(items)}")
+    print(f"part2 answer: {part2(items)}")
