a746f1267afe5ee1788e5d551aca17673748d2d5
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-14 23:33:01 -0700

    Progress.

--- 2024/day6.py
+++ 2024/day6.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from pprint import pp
 from typing import Tuple
 
@@ -25,7 +26,7 @@ TURNS = {
 def part1(items):
     pp(items)
     min_size = min(len(items), len(items[0]))
-    print(f'min_size: {min_size}')
+    print(f"min_size: {min_size}")
 
     position = find_guard(items)
     x, y = position
@@ -33,15 +34,15 @@ def part1(items):
     start_val = direction
     positions = [position]
     while True:
-        print(f'pos={position} dir={direction}')
+        print(f"pos={position} dir={direction}")
         positions.append(position)
         directive = MOVES[direction]
         i, j = next_position = move(position, directive)
-                
+
         if i < 0 or j < 0 or i > min_size or j > min_size:
             print(f"Guard has exited map at: items[{j}][{i}]")
             break
-        
+
         try:
             next_obstacle = items[j][i]
         except IndexError:
@@ -52,7 +53,7 @@ def part1(items):
             position = next_position
             continue
         elif next_obstacle == "#":
-            direction = TURNS[direction]        
+            direction = TURNS[direction]
 
     # pp(positions)
     ans = len(set(positions))
@@ -61,7 +62,45 @@ def part1(items):
 
 def part2(items):
     pp(items)
-    return
+    min_size = min(len(items), len(items[0]))
+    print(f"min_size: {min_size}")
+
+    position = find_guard(items)
+    x, y = position
+    direction = items[y][x]
+    start_val = direction
+    positions = [position]
+    while True:
+        print(f"pos={position} dir={direction}")
+        positions.append(position)
+        directive = MOVES[direction]
+        i, j = next_position = move(position, directive)
+
+        if i < 0 or j < 0 or i > min_size or j > min_size:
+            print(f"Guard has exited map at: items[{j}][{i}]")
+            break
+
+        try:
+            next_obstacle = items[j][i]
+        except IndexError:
+            print(f"Guard has exited map at: items[{j}][{i}]")
+            break
+
+        if next_obstacle == "." or next_obstacle == start_val:
+            position = next_position
+            continue
+        elif next_obstacle == "#":
+            direction = TURNS[direction]
+
+    travelled = deepcopy(items)
+    for position in positions:
+        x, y = position
+        travelled[y][x] = "X"
+
+    # pp(positions)
+    pp(travelled)
+    ans = len(set(positions))
+    return ans
 
 
 def find_guard(grid: list) -> Tuple[int, int]:
@@ -80,11 +119,11 @@ def move(point: Tuple[int, int], direction: Tuple[int, int]) -> Tuple[int, int]:
 
 def read_input(year: int, day: int) -> list:
     with open(f"{year}/data/day{day}_input.txt", "r") as f:
-        items = f.read().splitlines()
+        items = [[*i] for i in f.read().splitlines()]
         return items
 
 
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
-    print(f"part1 answer: {part1(items)}")
-    # print(f"part2 answer: {part2(items)}")
+    # print(f"part1 answer: {part1(items)}")
+    print(f"part2 answer: {part2(items)}")
