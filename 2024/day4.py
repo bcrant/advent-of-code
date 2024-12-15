95d006f072455fb93c8d9c083ff2a4dc76b3453c
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-14 17:15:16 -0700

    Day4 2024. Complete. oof.

--- 2024/day4.py
+++ 2024/day4.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from pprint import pp
 from typing import Tuple, Union
 
@@ -21,9 +20,10 @@ MOVES = {
     "NE": MOVE_NE,
     "NW": MOVE_NW,
     "SE": MOVE_SE,
-    "SW": MOVE_SW
+    "SW": MOVE_SW,
 }
-OPPOSITE_DIRECTIONS = {
+DIAGONAL_MOVES = {"NE": MOVE_NE, "NW": MOVE_NW, "SE": MOVE_SE, "SW": MOVE_SW}
+OPPOSITE_DIRECTION = {
     "NE": "SW",
     "NW": "SE",
     "SW": "NE",
@@ -34,7 +34,10 @@ OPPOSITE_CHARACTER = {
     "S": "M",
 }
 
-def get_adjacent_values(grid: list[list[str]], curr_pos: Tuple[int, int]) -> dict[Tuple[int, int], Union[None, str]]:
+
+def get_adjacent_values(
+    grid: list[list[str]], curr_pos: Tuple[int, int]
+) -> dict[Tuple[int, int], Union[None, str]]:
     adjacent = {}
     for direction, directive in MOVES.items():
         next_pos = move(curr_pos, directive)
@@ -50,9 +53,9 @@ def get_adjacent_values(grid: list[list[str]], curr_pos: Tuple[int, int]) -> dic
             continue
         next_next_next_val = get_value_if_exists(grid, next_next_next_pos)
         adjacent[next_pos] = {
-            "direction": direction, 
-            "next_pos": next_pos, 
-            "next_val": next_val, 
+            "direction": direction,
+            "next_pos": next_pos,
+            "next_val": next_val,
             "next_next_pos": next_next_pos,
             "next_next_val": next_next_val,
             "next_next_next_pos": next_next_next_pos,
@@ -67,7 +70,10 @@ def is_negative(coords: Tuple[int, int]) -> bool:
         return True
     return False
 
-def get_value_if_exists(grid: list[list[str]], coords: Tuple[int, int]) -> Union[None, Tuple[int, int]]:
+
+def get_value_if_exists(
+    grid: list[list[str]], coords: Tuple[int, int]
+) -> Union[None, Tuple[int, int]]:
     x, y = coords
     try:
         return grid[y][x]
@@ -77,12 +83,7 @@ def get_value_if_exists(grid: list[list[str]], coords: Tuple[int, int]) -> Union
 
 def part1(items):
     items = [[i for i in it] for it in items]
-    pp(items)
-
-    # 1. Initialize a counter
     cnt = 0
-
-    # 2.
     for y, row in enumerate(items):
         for x, val in enumerate(row):
             curr_pos = (x, y)
@@ -91,7 +92,7 @@ def part1(items):
                 adjacent = get_adjacent_values(items, curr_pos)
                 for _, v in adjacent.items():
                     if (
-                        v["next_val"] == "M" 
+                        v["next_val"] == "M"
                         and v["next_next_val"] == "A"
                         and v["next_next_next_val"] == "S"
                     ):
@@ -99,61 +100,49 @@ def part1(items):
     return cnt
 
 
-def get_immediate_adjacent_values(grid: list[list[str]], curr_pos: Tuple[int, int]) -> dict[Tuple[int, int], Union[None, str]]:
-    adjacent = {}
-    for direction, directive in MOVES.items():
-        next_pos = move(curr_pos, directive)
-        if is_negative(next_pos):
-            continue
-        next_val = get_value_if_exists(grid, next_pos)
-        adjacent[next_pos] = {
-            "direction": direction, 
-            "next_pos": next_pos, 
-            "next_val": next_val,
-        }
-    return adjacent
-
-
 def part2(items):
     items = [[i for i in it] for it in items]
-    pp(items)
-
-    # 1. Initialize a counter
     cnt = 0
-
-    # 2.
     for y, row in enumerate(items):
         for x, val in enumerate(row):
+            cnt_two = 0
             curr_pos = (x, y)
             curr_val = val
             if curr_val == "A":
                 adjacent = get_immediate_adjacent_values(items, curr_pos)
-                pp(adjacent)
-                for _, v in adjacent.items():
-                    direction = v["direction"]
-                    value = v["next_val"]
-                    is_opposite = is_opposite_valid(direction, value)
-                    if (
-                        True 
-                        and ((v["direction"] == "NE" and v["next_val"] == "M") and (v["direction"] == "SW" and v["next_val"] == "S"))
-                        and ((v["direction"] == "NW" and v["next_val"] == "M") and (v["direction"] == "SE" and v["next_val"] == "S"))
-                    ):
+                for d in ["NE", "NW"]:
+                    v = adjacent.get(d)
+                    if v not in OPPOSITE_CHARACTER.keys():
+                        break
+                    is_opposite = is_opposite_valid(d, v, adjacent)
+                    if is_opposite:
+                        cnt_two += 1
+                    if cnt_two == 2:
                         cnt += 1
     return cnt
 
 
-OPPOSITE_DIRECTIONS = {
-    "NE": "SW",
-    "NW": "SE",
-    "SW": "NE",
-    "SE": "NW",
-}
-OPPOSITE_CHARACTER = {
-    "M": "S",
-    "S": "M",
-}
-def is_opposite_valid(d: str, v: str) -> bool:
-    opposite_v = OPPOSITE_DIRECTIONS
+def is_opposite_valid(d: str, v: str, items: dict) -> bool:
+    opposite_d = OPPOSITE_DIRECTION[d]
+    opposite_v = OPPOSITE_CHARACTER[v]
+    is_opposite = items.get(opposite_d) == opposite_v
+    if is_opposite:
+        return True
+    return False
+
+
+def get_immediate_adjacent_values(
+    grid: list[list[str]], curr_pos: Tuple[int, int]
+) -> dict[str, str]:
+    adjacent = {}
+    for direction, directive in DIAGONAL_MOVES.items():
+        next_pos = move(curr_pos, directive)
+        if is_negative(next_pos):
+            continue
+        next_val = get_value_if_exists(grid, next_pos)
+        adjacent[direction] = next_val
+    return adjacent
+
 
 def move(point: Tuple[int, int], direction: Tuple[int, int]) -> Tuple[int, int]:
     """Apply a move in one direction"""
@@ -173,5 +162,5 @@ def read_input(year: int, day: int) -> list:
 
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
-    # print(f"part1 answer: {part1(items)}")
+    print(f"part1 answer: {part1(items)}")
     print(f"part2 answer: {part2(items)}")
