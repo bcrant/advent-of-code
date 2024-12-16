9d324cf37e246a91d51931eb9bb1a93b0354af1e
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-15 22:31:29 -0700

    Day6 2024.

--- 2024/day6.py
+++ 2024/day6.py
@@ -61,7 +61,8 @@ def part1(items):
 
 def part2(items):
     # pp(items)
-    for i in items: print("".join(i))
+    for i in items:
+        print("".join(i))
     print()
 
     backup_items = deepcopy(items)
@@ -83,7 +84,7 @@ def part2(items):
         if i < 0 or j < 0 or i >= min_size or j >= min_size:
             print(f"Guard has exited map at: (x={i}, y={j})")
             break
-        
+
         next_obstacle = items[j][i]
         if next_obstacle == ".":
             position = next_position
@@ -100,14 +101,9 @@ def part2(items):
             print(f"Guard has exited map at: (x={i}, y={j})")
             break
 
-
     plot_moves(visited, turns, start_pos, start_val)
     ans = len(set(visited))
     return ans
-
-
-
-
 
 
 def search_grid(grid: list, search_keys: List[str]) -> List[Tuple[int, int]]:
@@ -144,12 +140,14 @@ def plot_moves(positions, turns, start_pos, start_val) -> None:
 
     # print()
     # print(f'positions {type(positions)}')
-    for p in positions: print(p)
+    for p in positions:
+        print(p)
 
     print()
     travelled = ["".join(i) for i in travelled]
-    print(f'travelled {type(travelled)}')
-    for t in travelled: print(t)
+    print(f"travelled {type(travelled)}")
+    for t in travelled:
+        print(t)
 
 
 def read_input(year: int, day: int) -> list:
@@ -158,7 +156,100 @@ def read_input(year: int, day: int) -> list:
         return items
 
 
+def read_input_tapout(year: int, day: int) -> list:
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+        return items
+
+
+def tapout_part2(items):
+    """
+    Source: https://github.com/matheusstutzel/adventOfCode/blob/main/2024/06/p2.py
+    """
+    items = read_input_tapout(YEAR, DAY)
+
+    def test(map, pos, dir, options, ob):
+        #    print("testing", ob)
+        visit = set()
+
+        while (pos[0] >= 0 and pos[0] < len(map)) and (
+            pos[1] >= 0 and pos[1] < len(map[pos[0]])
+        ):
+            visit.add((pos[0], pos[1], dir))
+            (nextDir, delta) = options[dir]
+            ni = pos[0] + delta[0]
+            nj = pos[1] + delta[1]
+            if (ni, nj, dir) in visit:
+                # print(map)
+                return True
+
+            if ni < 0 or ni >= len(map) or nj < 0 or nj >= len(map[ni]):
+                pos = (ni, nj)
+                continue
+            if map[ni][nj] == "#" or (ni, nj) == ob:
+                # print((ni,nj)==ob, ob)
+                dir = nextDir
+                continue
+            pos = (ni, nj)
+        return False
+
+    # map = [l.strip() for l in sys.stdin]
+    # map = [l.strip() for l in sys.stdin]
+    map = items
+
+    options = {
+        "^": (">", (-1, 0)),
+        ">": ("v", (0, 1)),
+        "v": ("<", (1, 0)),
+        "<": ("^", (0, -1)),
+    }
+
+    pos = (0, 0)
+    dir = ">"
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] in options:
+                pos = (i, j)
+                dir = map[i][j]
+                break
+
+    initpos = pos
+    initdir = dir
+    while (pos[0] >= 0 and pos[0] < len(map)) and (
+        pos[1] >= 0 and pos[1] < len(map[pos[0]])
+    ):
+        line = map[pos[0]]
+        line = line[: pos[1]] + "X" + line[pos[1] + 1 :]
+        map[pos[0]] = line
+        (nextDir, delta) = options[dir]
+        ni = pos[0] + delta[0]
+        nj = pos[1] + delta[1]
+
+        if ni < 0 or ni >= len(map) or nj < 0 or nj >= len(map[ni]):
+            pos = (ni, nj)
+            continue
+        if map[ni][nj] == "#":
+            dir = nextDir
+            continue
+        pos = (ni, nj)
+
+    ob = []
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == "X":
+                ob.append((i, j))
+    print(len(ob))
+
+    count = 0
+    for x in ob:
+        if test(map, initpos, initdir, options, x):
+            # print(x)
+            count += 1
+    print(count)
+
+
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
     # print(f"part1 answer: {part1(items)}")
-    print(f"part2 answer: {part2(items)}")
+    # print(f"part2 answer: {part2(items)}")
+    print(f"part2 answer: {tapout_part2(items)}")
