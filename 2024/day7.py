b6b6f266e73ba5108a52e2bfc9b62a5451f8f89e
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-17 21:03:47 -0700

    First crack at Day7.

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,12 +1,33 @@
+import itertools
+import math
 from pprint import pp
+from typing import Dict, List
 
 
 YEAR, DAY = 2024, 7
 
 
-def part1(items):
-    pp(items)
-    return
+def part1(items: Dict[int, List[int]]):
+    ans = 0
+    for k, v in items.items():
+        add_pos = ["+" for _ in range(len(v))]
+        mul_pos = ["*" for _ in range(len(v))]
+        print(f'add_pos {type(add_pos)}: {add_pos}')
+        print(f'mul_pos {type(mul_pos)}: {mul_pos}')
+
+        combos = list(itertools.product(add_pos, mul_pos))
+        pp(combos)
+
+        # Try adding all
+        if sum(v) == k:
+            ans += k
+        # Try multiplying all
+        if math.prod(v) == k:
+            ans += k
+        # Try permutations of adding * multiplying
+        
+        series = sum(v)
+    return ans
 
 
 def part2(items):
@@ -17,6 +38,8 @@ def part2(items):
 def read_input(year: int, day: int) -> list:
     with open(f"{year}/data/day{day}_input.txt", "r") as f:
         items = f.read().splitlines()
+        items = [item.split(": ") for item in items]
+        items = {int(k): list(map(int, v.split())) for k, v in items}
         return items
 
 
