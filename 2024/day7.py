4a6df518913eaa3306c73622bcded348e61c69cf
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-19 17:29:49 -0700

    Checkpoint.

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,34 +1,44 @@
 import itertools
-import math
 from pprint import pp
 from typing import Dict, List
 
 
 YEAR, DAY = 2024, 7
+ADD = "+"
+MUL = "*"
 
 
 def part1(items: Dict[int, List[int]]):
+    ops = [ADD, MUL]
     ans = 0
     for k, v in items.items():
-        print(f'v: {v}')
-        # Try adding all
-        if sum(v) == k:
-            ans += k
-        # Try multiplying all
-        if math.prod(v) == k:
-            ans += k
-        # Try permutations of adding * multiplying
-        pairs = [[val, v[idx+1]] for idx, val in enumerate(v[0:-1])]
-        print(f'p: {pairs}')
-        adds = [sum(pair) for pair in pairs]
-        print(f'a: {adds}')
-        muls = [math.prod(pair) for pair in pairs]
-        print(f'm: {muls}')
-        combos = [*adds, *muls]
-        # combos = list(itertools.product(adds, muls))
-        combos = itertools.combinations()
-        pp(combos)
+        repeat = len(v) - 1
+        combos = list(itertools.product(ops, repeat=repeat))
+        for combo in combos:
+            op = []
+            for i in range(len(v)):
+                op.append(v[i])
+                try:
+                    op.append(combo[i])
+                except IndexError:
+                    pass
 
+            print(f'op: {op}')
+
+            # This uses precedence rules. The question specifies NOT to use PEDMAS.
+            # Compute from left to right instead.
+            # result = eval(''.join(op))
+            # print(f'op: {op} -> {result} || k={k} k==result ? {k==result}')
+                        
+            while len(op) >= 3:
+                result = str(eval(''.join(op[0:3])))
+                op = [result, *op[3:]]
+                # print(f'op: {op}')
+            _sum = int(op[0])
+            print(f'_sum: {_sum} eq {int(k)}')
+            if _sum == int(k):
+                ans += int(k)
+            # break
     return ans
 
 
@@ -41,11 +51,12 @@ def read_input(year: int, day: int) -> list:
     with open(f"{year}/data/day{day}_input.txt", "r") as f:
         items = f.read().splitlines()
         items = [item.split(": ") for item in items]
-        items = {int(k): list(map(int, v.split())) for k, v in items}
+        # items = {int(k): list(map(int, v.split())) for k, v in items}
+        items = {k: v.split() for k, v in items}
         return items
 
 
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
-    print(f"part1 answer: {part1(items)}")
+    print(f"part1 answer: {part1(items)}")  # 3749
     # print(f"part2 answer: {part2(items)}")
