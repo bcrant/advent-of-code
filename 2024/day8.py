aaeb23f1c80feaf7a55ae970439cf27639b3db97
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-20 17:40:28 -0500

    Checkpoint.

--- 2024/day8.py
+++ 2024/day8.py
@@ -1,4 +1,19 @@
+"""
+ORDER OF OPERATIONS
+- Find all unique characters
+- Find all occurrences of the unique characters
+- Determine all pairs and distance between pairs
+- Use distance between pairs to generate antinode locations.
+- If antinode in map boundary, add one to accumulator.
+
+CAVEATS
+- Antinodes only count if they are inside the boundary of the map
+- Antinodes can occur at the same location as an antenna
+"""
+
+import itertools
 from pprint import pp
+from typing import Tuple
 
 
 YEAR, DAY = 2024, 8
@@ -6,26 +21,37 @@ YEAR, DAY = 2024, 8
 AN = "#"
 
 def part1(items):
-    # Any lower/uppercase character or digit is an antenna
-    # Find pairs of antennas that are on the same line in any direction
-    # Determine if pairs are on a line
-    # Determine distance between antennas
-    #
-    # ORDER OF OPERATIONS
-    # - Brute force
-    #   - Find all unique characters
-    #   - Find all occurrences of the unique characters
-    #
-    # CAVEATS
-    # - Antinodes only count if they are inside the boundary of the map
-    # - Antinodes can occur at the same location as an antenna
     pp(items)
+    # Find all unique characters
+    antennas = [c for c in set(''.join(items)) if c != "."]
+    print(f'antennas: {antennas}')
+    
+    # Find all occurrences of the unique characters
+    locations = {k: [] for k in antennas}
+    for y, row in enumerate(items):
+        for x, val in enumerate(row):
+            if val == ".":
+                continue
+            locations[val].append((x, y))
+    pp(locations)
+
+    # Determine all pairs and distance between pairs
+    combos = {}
+    for k, v in locations.items():
+        combos[k] = list(itertools.combinations(v, r=2))
+    pp(combos)
     return
 
 
 def part2(items):
     pp(items)
     return
+
+
+def get_distance(p1: Tuple[int, int], p2: Tuple[int, int]) -> Tuple[int, int]:
+    x = abs(p2[0] - p1[0])
+    y = abs(p2[1] - p2[1])
+    return (x, y)
 
 
 def read_input(year: int, day: int) -> list:
@@ -36,5 +62,5 @@ def read_input(year: int, day: int) -> list:
 
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
-    print(f"part1 answer: {part1(items)}")
+    print(f"part1 answer: {part1(items)} (test_answer=14)")
     # print(f"part2 answer: {part2(items)}")
