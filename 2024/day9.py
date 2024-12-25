310f9064a4fa23f48a9c31722aed1155e95ecf53
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-25 09:48:25 -0500

    Checkpoint.

--- 2024/day9.py
+++ 2024/day9.py
@@ -14,6 +14,7 @@ from pprint import pp
 
 
 YEAR, DAY = 2024, 9
+FREE = "."
 
 
 def part1(items):
@@ -26,31 +27,68 @@ def part1(items):
     while idx < len(items):
         file_size = items[idx]
         try:
-            free_space = items[idx+1]
+            free_space = items[idx + 1]
         except IndexError:
             free_space = 0
         files[(_id, int(file_size))] = int(free_space)
         idx += 2
         _id += 1
-    print_disk_map(files)
+
+    disk_map = generate_disk_map(files)
 
     # Get file_ids
-    file_ids = [k[0] for k in files.keys()]
-    print(f'file_ids {type(file_ids)}: {file_ids}')
+    file_id_lkp = {k[0]: k for k in files.keys()}
+    file_ids = list(reversed(file_id_lkp.keys()))
+
+    total_free = disk_map.count(".")
+    free_block = FREE * disk_map.count(".")
 
     # Recursively move from end to empty space
-    
-    return
+    # - get count of free space
+    # - create string of free space
+    # - while string of free space not in disk map, do...
+    # - get index of first free space
+    # - get index of last file_id (integer)
+    # - pop file_id
+    # - insert file_id
 
-def print_disk_map(files: dict) -> None:
-    disk_map = ''
+    dm = [str(i) for i in disk_map]
+    print(''.join(dm))
+
+    for file_id in file_ids:
+        _id, _len = file_id_lkp[file_id]
+        for _ in range(0, _len):
+            free_idx = dm.index(FREE)
+            # Does this insert overwrite or insert?
+            dm[free_idx] = str(_id)
+            dm.append(FREE)
+            print(''.join(dm))
+
+            rm_idx = ''.join(dm).rfind(str(_id))
+            if rm_idx > len(dm):
+                print(f'rm_idx {type(rm_idx)}: {rm_idx}')
+                continue
+            else:
+                dm.pop(rm_idx)
+
+    # Checksum
+    checksum = 0
+    for idx, _id in enumerate(dm):
+        if _id != ".":
+            checksum += idx * int(_id)
+    return checksum
+
+
+def generate_disk_map(files: dict) -> str:
+    disk_map = ""
     for k, v in files.items():
         _id, _len = k
-        free = "." * v
+        free = FREE * v
         disk_map += f"{str(_id) * _len}{free}"
     print()
-    print(f'disk_map:')
+    print(f"disk_map:")
     print(disk_map)
+    return disk_map
 
 
 def part2(items):
@@ -66,5 +104,5 @@ def read_input(year: int, day: int) -> list:
 
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
-    print(f"part1 answer: {part1(items)}")
+    print(f"part1 answer: {part1(items)}") # test answer=1928
     # print(f"part2 answer: {part2(items)}")
