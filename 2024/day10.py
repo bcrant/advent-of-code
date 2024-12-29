ed79ad1cc84fc1dfb4a18c0d751dbc356000afe8
Brian Crant <17074379+bcrant@users.noreply.github.com>
2024-12-29 11:41:51 -0700

    Day10 2024 complete.

--- 2024/day10.py
+++ 2024/day10.py
@@ -5,8 +5,8 @@ from typing import List, Tuple
 
 YEAR, DAY = 2024, 10
 
-START = '0'
-END = '9'
+START = "0"
+END = "9"
 
 MOVE_E = MOVE_EAST = (1, 0)
 MOVE_N = MOVE_NORTH = (0, -1)
@@ -20,31 +20,10 @@ MOVES = {
 }
 
 
-def bfs(graph, start):
-    visited = set()  # Set to track visited nodes
-    queue = deque([start])  # Initialize the queue with the start node
-    result = []  # List to store the order of traversal
-
-    while queue:
-        # Dequeue a vertex from the queue
-        current_node = queue.popleft()
-
-        if current_node not in visited:
-            # Mark the node as visited and process it
-            visited.add(current_node)
-            result.append(current_node)
-
-            # Enqueue all adjacent unvisited nodes
-            for neighbor in graph[current_node]:
-                if neighbor not in visited:
-                    queue.append(neighbor)
-    return result
-
-
 def part1(items):
-    pp(items)
+    # pp(items)
     start_positions: List[int, int] = []
-    end_positions: List[int, int]   = []
+    end_positions: List[int, int] = []
     for y, row in enumerate(items):
         for x, val in enumerate(row):
             if val == START:
@@ -52,99 +31,76 @@ def part1(items):
             elif val == END:
                 end_positions.append((x, y))
 
-    cnt = 0
-    trails = defaultdict(dict)
+    trails = defaultdict(int)
     for start_pos in start_positions:
-        visited = set()             # Set to track visited nodes
-        queue = deque([start_pos])  # Initialize the queue with the start node
-        result = []                 # List to store the order of traversal
-        print(f'result: {result}')
-    
+        queue = deque([start_pos])
+        visited = set()
+        reachable = set()
+
         while queue:
             # Dequeue a vertex from the queue
             current_node = queue.popleft()
-            print(f'current_node: {current_node}')
-
-            # Base case: Reached target
-            if current_node in end_positions:
-                cnt += 1
-                continue
 
             if current_node not in visited:
                 # Mark the node as visited and process it
                 visited.add(current_node)
-                result.append(current_node)
 
-                # Enqueue all adjacent unvisited nodes
-                neighbors = get_valid_adjacent_moves(items, current_node)
-                for neighbor in neighbors:
-                    
-                    # Base case: Reached target
-                    if neighbor in end_positions:
-                        # visited.add(neighbor)
-                        cnt += 1
-                        continue
+            # Base case: Reached target
+            if current_node in end_positions:
+                reachable.add((start_pos, current_node))
 
-                    if neighbor not in visited:
-                        queue.append(neighbor)
-        print(f'result: {result}')
+            # Enqueue all adjacent unvisited nodes
+            neighbors = get_valid_adjacent_moves(items, current_node)
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    queue.append(neighbor)
 
-        # curr_pos = start_pos
-        # trail = []
-        # while True:
-        #     # Base case: Reached target
-        #     if curr_pos in end_positions:
-        #         trails[curr_pos][valid_move]
-        #         cnt += 1
-        #         continue
-            
-        #     valid_moves = get_valid_adjacent_moves(items, curr_pos)
+        trails[start_pos] += len(reachable)
 
-        #     # Base case: Ran out of moves without reaching target
-        #     if not valid_moves:
-        #         break
-
-        #     for valid_move in valid_moves:
-        #         print(f'valid_move {type(valid_move)}: {valid_move}')
-
-        #         # Base case: Reached target
-        #         if valid_move in end_positions:
-        #             trails[curr_pos][valid_move]
-        #             cnt += 1
-        #             continue
-
-        #         trails[curr_pos][valid_move] = {}
-        #         curr_pos = valid_move
-            
-        #     break
-    print()
-    print(f'start_positions : {start_positions}')
-    print(f'end_positions   : {end_positions}')
-    print()
-
-    print()
-    print('Trails...')
-    pp(trails)
-    print()
-
-    return cnt
+    ans = sum(trails.values())
+    return ans
 
 
 def part2(items):
-    pp(items)
-    return
+    # pp(items)
+    start_positions: List[int, int] = []
+    end_positions: List[int, int] = []
+    for y, row in enumerate(items):
+        for x, val in enumerate(row):
+            if val == START:
+                start_positions.append((x, y))
+            elif val == END:
+                end_positions.append((x, y))
 
+    trails = defaultdict(int)
+    for start_pos in start_positions:
+        queue = deque([start_pos])
+        visited = set()
+        reachable = list()
 
-# def get_valid_adjacent_moves(grid: list[list[str]], curr_pos: Tuple[int, int]) -> list:
-#     x, y = curr_pos
-#     elevation = int(grid[y][x])
-#     adjacent = get_adjacent_moves(items, curr_pos)
-#     valid_moves = [
-#         next_pos
-#         for _direction, next_pos, next_val in adjacent
-#         if next_val == elevation + 1
-#     ]
-#     return valid_moves
+        while queue:
+            # Dequeue a vertex from the queue
+            current_node = queue.popleft()
+            # print(f'current_node: {current_node}')
+
+            if current_node not in visited:
+                # Mark the node as visited and process it
+                visited.add(current_node)
+
+            # Base case: Reached target
+            if current_node in end_positions:
+                reachable.append((start_pos, current_node))
+
+            # Enqueue all adjacent unvisited nodes
+            neighbors = get_valid_adjacent_moves(items, current_node)
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+        trails[start_pos] += len(reachable)
+
+    ans = sum(trails.values())
+    return ans
 
 
 def get_valid_adjacent_moves(grid: list[list[str]], curr_pos: Tuple[int, int]) -> list:
@@ -152,11 +108,10 @@ def get_valid_adjacent_moves(grid: list[list[str]], curr_pos: Tuple[int, int]) -
     elevation = int(grid[y][x])
     adjacent = get_adjacent_moves(items, curr_pos)
     valid_moves = [
-        next_pos
-        for _, next_pos, next_val in adjacent
-        if next_val == elevation + 1
+        next_pos for _, next_pos, next_val in adjacent if next_val == elevation + 1
     ]
     return valid_moves
+
 
 def get_adjacent_moves(grid: list[list[str]], curr_pos: Tuple[int, int]) -> list:
     boundaries = (len(grid[0]), len(grid))
@@ -197,4 +152,4 @@ def read_input(year: int, day: int) -> list:
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
     print(f"part1 answer: {part1(items)}")
-    # print(f"part2 answer: {part2(items)}")
+    print(f"part2 answer: {part2(items)}")
