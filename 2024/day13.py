cfe7d47f426575ae608c299d0c0882620a1e423f
Brian Crant <17074379+bcrant@users.noreply.github.com>
2025-01-17 13:37:03 -0800

    Missed limit of 100 button presses per button.

--- 2024/day13.py
+++ 2024/day13.py
@@ -1,38 +1,34 @@
 import math
 import re
 from collections import namedtuple
-from itertools import product
 from pprint import pp
 
 
 YEAR, DAY = 2024, 13
 TOKENS_A = 3
 TOKENS_B = 1
+MAX_BUTTON_PRESSES = 100
 
 ClawMachine = namedtuple("ClawMachine", ["a", "b", "p"])
 
-# The cheapest way to win the prize is by pushing the A button 80 times and the B button 40 times. This would line up the claw along the X axis (because 80*94 + 40*22 = 8400) and along the Y axis (because 80*34 + 40*67 = 5400). Doing this would cost 80*3 tokens for the A presses and 40*1 for the B presses, a total of 280 tokens.
-
 
 def part1(items):
-    result = {}
+    tokens = 0
     for item in items:
-        print(f'item {type(item)}: {item}')
-        result[str(item)] = route_machine(item)
-    pp(result)
-    return result
+        print()
+        print(item)
+        tokens += route_machine(item) or 0
+    return tokens
 
 
 def route_machine(item):
-    print(item)
-
+    """Solve for...
+    (ax * k) + (bx * j) = px
+    (ay * m) + (by * n) = py
+    """
     ax, ay = item.a
     bx, by = item.b
     px, py = item.p
-
-    # Solve for...
-    # (ax * k) + (bx * j) = px
-    # (ay * m) + (by * n) = py
 
     axx = math.ceil(px / ax)
     ayy = math.ceil(py / ay)
@@ -43,29 +39,10 @@ def route_machine(item):
     print(f"ayy = py{py} / ay{ay} = {ayy}")
     print(f"byy = py{py} / by{ay} = {byy}")
 
-
-    ax_maps = {
-        n*ax: n
-        for n in range(0, axx)
-    }    
-    bx_maps = {
-        n*bx: n
-        for n in range(0, bxx)
-    }
-    ay_maps = {
-        n*ay: n
-        for n in range(0, ayy)
-    }    
-    by_maps = {
-        n*by: n
-        for n in range(0, byy)
-    }
-
-
-    # Prize X position is evenly divisible by A or B button alone.
-    if px in ax_maps.keys() or px in bx_maps.keys():
-        match = ax_maps[px] or bx_maps[px]
-        print(f"Exact match! x = {match}")
+    ax_maps = {n * ax: n for n in range(0, axx)}
+    bx_maps = {n * bx: n for n in range(0, bxx)}
+    ay_maps = {n * ay: n for n in range(0, ayy)}
+    by_maps = {n * by: n for n in range(0, byy)}
 
     # Brute force combinations of A and B button presses
     x_tokens = {}
@@ -75,15 +52,11 @@ def route_machine(item):
             if cx == px:
                 ab = ax_maps[ax]
                 bb = bx_maps[bx]
-                total_tokens = (ab * TOKENS_A) + (bb * TOKENS_B)
-                # print(f"Combined match! ax+bx=cx {ax}+{bx}={cx} --> Button presses: (A={ab}, B={bb}) --> {total_tokens} tokens")
-                x_tokens[total_tokens] = (ab, bb)
+                if ab > MAX_BUTTON_PRESSES or bb > MAX_BUTTON_PRESSES:
+                    continue
+                cnt = (ab * TOKENS_A) + (bb * TOKENS_B)
+                x_tokens[cnt] = (ab, bb)
                 break
-
-    print(f'x_tokens {type(x_tokens)}:')
-    pp(x_tokens)
-    min_x_tokens = min(x_tokens.keys()) if x_tokens.keys() else None
-    print(f'min_x_tokens: {min_x_tokens}')
 
     y_tokens = {}
     for ay in ay_maps.keys():
@@ -92,17 +65,19 @@ def route_machine(item):
             if cy == py:
                 ab = ay_maps[ay]
                 bb = by_maps[by]
-                total_tokens = (ab * TOKENS_A) + (bb * TOKENS_B)
-                # print(f"Combined match! ay+by=cy {ay}+{by}={cy} --> Button presses: (A={ab}, B={bb}) --> {total_tokens} tokens")
-                y_tokens[total_tokens] = (ab, bb)
+                if ab > MAX_BUTTON_PRESSES or bb > MAX_BUTTON_PRESSES:
+                    continue
+                cnt = (ab * TOKENS_A) + (bb * TOKENS_B)
+                y_tokens[cnt] = (ab, bb)
                 break
 
-    print(f'y_tokens {type(y_tokens)}')
-    pp(y_tokens)
-    min_y_tokens = min(y_tokens.keys()) if y_tokens.keys() else None
-    print(f'min_y_tokens: {min_y_tokens}')
+    ans = 0
+    if not x_tokens.keys() and not y_tokens.keys():
+        return ans
 
-    ans = min([k for k in x_tokens.keys() if k in y_tokens.keys()])
+    matches = [k for k in x_tokens.keys() if k in y_tokens.keys()]
+    if matches:
+        ans = min(matches)
     return ans
 
 
