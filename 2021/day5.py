a3debcdfa0610f0005e2a160356e6c765142f8f4
Brian Crant <17074379+bcrant@users.noreply.github.com>
2025-02-17 15:49:13 -0700

    Day5 2021.

--- 2021/day5.py
+++ 2021/day5.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pprint import pp
 
 y, d = 2021, 5
@@ -57,8 +58,7 @@ def part1():
 
     return len(flattened_card)
 
-
-def part2():
+    # def part2():
     with open(f"{y}/data/day{d}_input.txt", "r") as f:
         # with open(f"{y}/data/day{d}_example.txt", "r") as f:
         items = f.read().splitlines()
@@ -127,6 +127,35 @@ def part2():
     # pp(flattened_card)
 
     return len(flattened_card)
+
+
+def part2():
+    with open(f"2021/data/day5_input.txt", "r") as f:
+        items = f.read().splitlines()
+
+    G1 = defaultdict(int)
+    G2 = defaultdict(int)
+    for line in items:
+        start, end = line.split("->")
+        x1, y1 = start.split(",")
+        x2, y2 = end.split(",")
+        x1 = int(x1.strip())
+        y1 = int(y1.strip())
+        x2 = int(x2.strip())
+        y2 = int(y2.strip())
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        for i in range(1 + max(abs(dx), abs(dy))):
+            x = x1 + (1 if dx > 0 else (-1 if dx < 0 else 0)) * i
+            y = y1 + (1 if dy > 0 else (-1 if dy < 0 else 0)) * i
+            if dx == 0 or dy == 0:
+                G1[(x, y)] += 1
+            G2[(x, y)] += 1
+
+    print(len([k for k in G1 if G1[k] > 1]))
+    print(len([k for k in G2 if G2[k] > 1]))
 
 
 if __name__ == "__main__":
---
+++ 2021/tmpl.py
@@ -0,0 +1,26 @@
+from pprint import pp
+
+
+YEAR, DAY = 2021, 1
+
+
+def part1(items):
+    pp(items)
+    return
+
+
+def part2(items):
+    pp(items)
+    return
+
+
+def read_input(year: int, day: int) -> list:
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+        return items
+
+
+if __name__ == "__main__":
+    items = read_input(YEAR, DAY)
+    print(f"part1 answer: {part1(items)}")
+    # print(f"part2 answer: {part2(items)}")
