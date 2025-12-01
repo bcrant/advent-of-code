e8c77c3ce03e6436fe0814420a3a37a91fd7b810
Brian Crant <17074379+bcrant@users.noreply.github.com>
2025-11-30 22:31:16 -0800

    yikes.

--- 2025/day1.py
+++ 2025/day1.py
@@ -5,13 +5,32 @@ YEAR, DAY = 2025, 1
 
 
 def part1(items):
-    pp(items)
-    return
+    cnt_z = 0
+    pos = 50
+    for item in items:
+        direction, turn = item[0], int(item[1:])
+        if direction == "L":
+            _, pos = divmod(pos-turn, 100)
+        if direction == "R":
+            _, pos = divmod(pos+turn, 100)
+        if pos == 0:
+            cnt_z += 1
+    return cnt_z
 
 
 def part2(items):
-    pp(items)
-    return
+    cnt_z = 0
+    pos = 50
+    for item in items:
+        direction, turn = item[0], int(item[1:])
+        for i in range(turn):
+            if direction == "L":
+                pos = (pos - 1 + 100) % 100
+            if direction == "R":
+                pos = (pos + 1 ) % 100
+            if pos == 0:
+                cnt_z += 1
+    return cnt_z
 
 
 def read_input(year: int, day: int) -> list:
@@ -23,4 +42,4 @@ def read_input(year: int, day: int) -> list:
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
     print(f"part1 answer: {part1(items)}")
-    # print(f"part2 answer: {part2(items)}")
+    print(f"part2 answer: {part2(items)}")
