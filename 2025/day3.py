3896723a5a4c7e5e2cea0ea42e86fab214644d9e
Brian Crant <17074379+bcrant@users.noreply.github.com>
2025-12-02 23:16:40 -0800

    Day 3 2025.

--- 2025/day3.py
+++ 2025/day3.py
@@ -21,51 +21,50 @@ def part1(items):
     return cnt
 
 
-CACHE = {}
-
 def part2(items):
     cnt = 0
     banks = items
-    CACHE = {}
     for bank in banks:
-        cnt += memoize(bank, 0, 0)
+
         CACHE = {}
+
+        def memoize(bank, i, seen):
+            """
+            Taken from Jonathan Paulson, reworded and commented to help my understanding of this technique.
+            https://github.com/jonathanpaulson/AdventOfCode/blob/master/2025/3.py
+            """
+            if i == len(bank) and seen == 12:
+                # base case, we have picked 12 digits
+                return 0
+            if i == len(bank):
+                # return invalid if we reach the end and have picked less than 12 digits
+                return -10 ** 20
+            
+            # return from cache if exists, this is how we avoid computing trillions+ of combinations
+            cache_key = (i, seen)
+            if cache_key in CACHE:
+                return CACHE[cache_key]
+            
+            # compute next digit without choosing it
+            ans = memoize(bank, i+1, seen)
+
+            # take existing sequence, adds current digit, then zero fills to the right
+            # to produce a 12 digit number. This works since it doesn't matter what numbers are 
+            # to the right of current sequence, so long as there are 12 - i. This increases
+            # the current maximum faster, allowing us to compute less combinations.
+            if seen < 12:
+                ans = max(
+                    ans,
+                    10 ** (11 - seen) * int(bank[i]) + memoize(bank, i+1, seen+1)
+                )
+            
+            # memoize result in return
+            CACHE[cache_key] = ans
+            return ans
+
+        cnt += memoize(bank, 0, 0)
+
     return cnt
-
-
-def memoize(bank, i, seen):
-    """
-    Taken from Jonathan Paulson, reworded and commented to help my understanding of this technique.
-    https://github.com/jonathanpaulson/AdventOfCode/blob/master/2025/3.py
-    """
-    if i == len(bank) and seen == 12:
-        # base case, we have picked 12 digits
-        return 0
-    if i == len(bank):
-        # return invalid if we reach the end and have picked less than 12 digits
-        return -10 ** 20
-    
-    # return from cache if exists, this is how we avoid computing trillions+ of combinations
-    cache_key = (i, seen)
-    if cache_key in CACHE:
-        return CACHE[cache_key]
-    
-    # compute next digit without choosing it
-    ans = memoize(bank, i+1, seen)
-
-    # take existing sequence, adds current digit, then zero fills to the right
-    # to produce a 12 digit number. This works since it doesn't matter what numbers are 
-    # to the right of current sequence, so long as there are 12 - i. This increases
-    # the current maximum faster, allowing us to compute less combinations.
-    if seen < 12:
-        ans = max(
-            ans,
-            10 ** (11 - seen) * int(bank[i]) + memoize(bank, i+1, seen+1)
-        )
-    
-    # memoize result in return
-    CACHE[cache_key] = ans
-    return ans
 
 
 def read_input(year: int, day: int) -> list:
@@ -76,14 +75,5 @@ def read_input(year: int, day: int) -> list:
 
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
-    # print(f"part1 answer: {part1(items)}")
+    print(f"part1 answer: {part1(items)}")
     print(f"part2 answer: {part2(items)}")
-    # cnt = 0
-    # banks = items
-    # for bank in banks:
-    #     CACHE = {}
-    #     cnt += memoize(bank, 0, 0)
-    # # 130504248624200
-    # # 168617068915447
-    # print(f'cnt {type(cnt)}: {cnt}')
-
