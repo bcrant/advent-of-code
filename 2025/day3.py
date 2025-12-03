9fbe53f564c0eb00db53c018fea851fea0aa16f5
Brian Crant <17074379+bcrant@users.noreply.github.com>
2025-12-02 21:57:05 -0800

    Checkpoint.

---
+++ 2025/day3.py
@@ -0,0 +1,60 @@
+import itertools
+from pprint import pp
+
+
+YEAR, DAY = 2025, 3
+
+
+def part1(items):
+    pp(items)
+    cnt = 0
+    banks = items
+    for bank in banks:
+        print(f'bank {type(bank)}: {bank}')
+        pairs = set()
+        j = 0
+        while j < len(bank)-1:
+            for k in range(j+1, len(bank)):
+                pair = int(bank[j]+bank[k])
+                pairs.add(pair)
+            j += 1
+        max_jolts = max(pairs)
+        print(f'pairs {type(pairs)}: {pairs}')
+        print(f'max_j {type(max_jolts)}: {max_jolts}')
+        cnt += max(pairs)
+    return cnt
+
+
+def part2(items):
+    cnt = 0
+    banks = items
+    tiny_banks = []
+    for bank in banks:
+        # Remove smallest number until only 12 items remain to reduce combinatorics
+        for i in range(1, 10):
+            _bank = ''.join([_ for _ in bank if int(_) != i])
+            if len(_bank) >= 12:
+                bank = _bank
+            else:
+                tiny_banks.append(bank)
+                break
+
+    for bank in tiny_banks:
+        combos = set(
+            int(''.join(combo))
+            for combo in list(itertools.combinations(bank, 12))
+        )
+        cnt += max(combos)
+    return cnt
+
+
+def read_input(year: int, day: int) -> list:
+    with open(f"{year}/data/day{day}_input.txt", "r") as f:
+        items = f.read().splitlines()
+        return items
+
+
+if __name__ == "__main__":
+    items = read_input(YEAR, DAY)
+    # print(f"part1 answer: {part1(items)}")
+    print(f"part2 answer: {part2(items)}")
