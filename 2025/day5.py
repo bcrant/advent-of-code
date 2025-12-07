bc2e8472d2ba5b07eaf04f9dcb1f265f06608a17
Brian Crant <17074379+bcrant@users.noreply.github.com>
2025-12-06 21:36:02 -0800

    too slow.

--- 2025/day5.py
+++ 2025/day5.py
@@ -2,12 +2,19 @@ from pprint import pp
 
 
 IS_TEST = False
-YEAR, DAY = 2025, 1
+YEAR, DAY = 2025, 5
 
 
 def part1(items):
-    pp(items)
-    return
+    # pp(items)
+    fresh_id_ranges, inventory_ids = str(items).strip().split('\n\n')
+    fresh_id_ranges = [i.split("-") for i in fresh_id_ranges.split('\n')]
+    fresh_ids = set()
+    for x, y in fresh_id_ranges:
+        for i in range(int(x), int(y)+1):
+            fresh_ids.add(str(i))    
+    inventory_ids = set(inventory_ids.split('\n'))
+    return len(inventory_ids.intersection(fresh_ids))
 
 
 def part2(items):
@@ -18,7 +25,7 @@ def part2(items):
 def read_input(year: int, day: int) -> list:
     file_suffix = "input_test" if IS_TEST else "input" 
     with open(f"{year}/data/day{day}_{file_suffix}.txt", "r") as f:
-        items = f.read().splitlines()
+        items = f.read()
         return items
 
 
