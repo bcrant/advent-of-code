8d851111d363fec9014d554c01800985bac0c4b9
Brian Crant <17074379+bcrant@users.noreply.github.com>
2025-12-06 23:23:34 -0800

    2025 Day 5.

--- 2025/day5.py
+++ 2025/day5.py
@@ -1,4 +1,3 @@
-from pprint import pp
 
 
 IS_TEST = False
@@ -8,12 +7,12 @@ YEAR, DAY = 2025, 5
 def part1(items):
     # pp(items)
     fresh_id_ranges, inventory_ids = str(items).strip().split('\n\n')
-    fresh_id_ranges = {int(i.split("-")[0]): list(map(int, i.split("-"))) for i in fresh_id_ranges.split('\n')}
-    inventory_ids = {int(i): None for i in inventory_ids.split('\n')}
+    fresh_id_ranges = [list(map(int, i.split("-"))) for i in fresh_id_ranges.split('\n')]
+    inventory_ids = set(int(i) for i in inventory_ids.split('\n'))
     cnt = 0
-    for iid in inventory_ids.keys():
+    for iid in inventory_ids:
         seen = set()
-        for x, y in fresh_id_ranges.values():
+        for x, y in fresh_id_ranges:
             if iid not in seen and iid >= x and iid <= y:
                 seen.add(iid)
                 cnt += 1
@@ -21,8 +20,19 @@ def part1(items):
 
 
 def part2(items):
-    pp(items)
-    return
+    """You can avoid counting the duplicates caused by overlapping ranges by sorting
+    the ranges and incrementing the lower bound and counting ids in range."""
+    fresh_id_ranges, _ = str(items).strip().split('\n\n')
+    fresh_id_ranges = sorted([list(map(int, i.split("-"))) for i in fresh_id_ranges.split('\n')])
+    cnt = 0
+    curr = -1
+    for x, y in fresh_id_ranges:
+        if curr >= x:
+            x = curr + 1
+        if x <= y:
+            cnt += y - x + 1
+            curr = max(curr, y)
+    return cnt
 
 
 def read_input(year: int, day: int) -> list:
@@ -35,6 +45,4 @@ def read_input(year: int, day: int) -> list:
 if __name__ == "__main__":
     items = read_input(YEAR, DAY)
     print(f"part1 answer: {part1(items)}")
-    # 660 too low
-    # 774 too high
-    # print(f"part2 answer: {part2(items)}")
+    print(f"part2 answer: {part2(items)}")
